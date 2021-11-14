35b444c5b30fddc4e9b93bbe9427a5d77f7efee1
Minh Van Nguyen <van23112002@gmail.com>
2021-11-14 17:25:33 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,20 +1,81 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-import "react-pro-sidebar/dist/css/styles.css";
+import React, { useState } from "react";
+
+import {Navigation} from 'react-minimal-side-navigation';
+import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
+import Icon from "awesome-react-icons";
+import { Row, Col, Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
 
 function App() {
+  const [ data, setData ] = useState(0);
   return (
-    <ProSidebar>
-      <Menu iconShape="square">
-        <MenuItem >Dashboard</MenuItem>
-        <SubMenu title="Components" >
-          <MenuItem>Component 1</MenuItem>
-          <MenuItem>Component 2</MenuItem>
-        </SubMenu>
-      </Menu>
-    </ProSidebar>
+    <div>
+    <div>
+    <Row>
+      <div style={{width: '20rem'}} className="d-flex flex-column flex-shrink-0 p-0 text-white bg-light">
+        <Card style={{ height: '100vh', margin: 0, padding: 0 }}>
+          <Card.Body>
+        <Navigation 
+            // you can use your own router's api to get pathname
+            activeItemId="/management/members"
+            onSelect={({itemId}) => {
+              switch ({itemId}) {
+                case '/dashboard':
+                  setData(1);
+                  break;
+              
+                default:
+                  setData(2);
+                  break;
+              }
+              
+            }}
+            items={[
+              {
+                title: 'Dashboard',
+                itemId: '/dashboard',
+                // you can use your own custom Icon component as well
+                // icon is optional
+                elemBefore: () => <Icon name="inbox" />,
+              },
+              {
+                title: 'Management',
+                itemId: '/management',
+                elemBefore: () => <Icon name="users" />,
+                subNav: [
+                  {
+                    title: 'Projects',
+                    itemId: '/management/projects',
+                  },
+                  {
+                    title: 'Members',
+                    itemId: '/management/members',
+                  },
+                ],
+              },
+              {
+                title: 'Another Item',
+                itemId: '/another',
+                subNav: [
+                  {
+                    title: 'Teams',
+                    itemId: '/management/teams',
+                  },
+                ],
+              },
+            ]}
+          />
+          </Card.Body>
+        </Card>
+      </div>
+      <Col>{data==1 ? (<div>Hello</div>) : data==2 ? (<div>Hi</div>) : (<div>Default</div>)}</Col>
+    </Row>
+    </div>
+  </div>
   );
 }
+
 
 export default App;
